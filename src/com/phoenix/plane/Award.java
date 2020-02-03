94edd992b4965855c27f88380b71bbc7c7419f38
phoenix fate <sm516116978@outlook.com>
2020-02-03 23:25:26 +0800

    Initial commit

---
+++ src/com/phoenix/plane/Award.java
@@ -0,0 +1,13 @@
+package com.phoenix.plane;
+
+/**
+ * Created by Albee on 2017/5/13.
+ */
+public interface Award {
+
+
+    int DOUBLE_FIRE = 0;
+    int LIFE = 1;
+    int getType();
+
+}
---
+++ src/com/phoenix/plane/Enemy.java
@@ -0,0 +1,8 @@
+package com.phoenix.plane;
+
+/**
+ * Created by Albee on 2017/5/13.
+ */
+public interface Enemy {
+    int getScore();
+}
